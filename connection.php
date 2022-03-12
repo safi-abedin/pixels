17efec2e69f0c67ac03d6bfe51fc94cf7ff2c234
safi-abedin <66793554+safi-abedin@users.noreply.github.com>
2022-03-12 21:09:13 +0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,30 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "new";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$Username=$_POST['Email'];
+$Password=$_POST['Pass'];
+
+$sql = "INSERT INTO registration (Email,Pass)
+VALUES ('$Username',$Password)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+header('location:index.html');
+?>
